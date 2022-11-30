34b6c22d7c43bad16aa40c76d3690e893d1457aa
Cesar Figueroa <cesarabelfigueroa@gmail.com>
2022-11-30 15:22:56 -0300

    installing and testing octokit to do requests into github

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    let value = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      owner: 'octocat',
+      repo: 'Spoon-Knife',
+    });
+
+    console.log(value);
+    return 'Hello World2!';
   }
 }
