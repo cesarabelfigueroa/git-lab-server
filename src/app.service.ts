27698bb025117db41442b49260d12dfe92c52b42
Cesar Figueroa <cesarabelfigueroa@gmail.com>
2022-11-30 15:35:43 -0300

    get push events from github

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,12 +6,20 @@ const octokit = new Octokit();
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    let value = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-      owner: 'octocat',
-      repo: 'Spoon-Knife',
-    });
+    let response = await octokit.request(
+      'GET /repos/{owner}/{repo}/events{?per_page,page}',
+      {
+        owner: 'cesarabelfigueroa',
+        repo: 'git-lab-server',
+        page: 1,
+      },
+    );
 
-    console.log(value);
-    return 'Hello World2!';
+    if (response && response.status === 200) {
+      let pushEvents  = response?.data?.filter((event) => event.type === 'PushEvent')
+      return JSON.stringify(pushEvents || []);
+    }
+
+    return JSON.stringify(response);
   }
 }
